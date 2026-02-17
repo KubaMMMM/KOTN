5bcc94faecfa735318e336a0614b2670c8181489
Kuba <janhra155@gmail.com>
2026-02-17 16:23:11 +0100

    debugging 7

--- src/cz/macek/knight/main/Main.java
+++ src/cz/macek/knight/main/Main.java
@@ -2,14 +2,12 @@ package cz.macek.knight.main;
 
 import java.util.Scanner;
 
-class Main {
+public class Main {
     public static void main(String[] args){
 
 
         UI ui = new UI();
         ui.gameStart();
 
-
-        //TODO: udelat jarj
     }
 }
