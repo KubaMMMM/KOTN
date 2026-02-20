16acf02c48aa5a047a67aef12d34f292dae194e3
Kuba <janhra155@gmail.com>
2026-02-20 23:23:20 +0100

    debugging 12 (final)

--- src/cz/macek/knight/main/Main.java
+++ src/cz/macek/knight/main/Main.java
@@ -2,6 +2,15 @@ package cz.macek.knight.main;
 
 import java.util.Scanner;
 
+/**
+ * KNIGHT OF THE NINE
+ *
+ * Textová adventura v jazyce Java.
+ *
+ * Autor: Jakub Maček
+ * Škola: SPŠE Ječná
+ * Rok: 2026
+ */
 public class Main {
     public static void main(String[] args){
 
